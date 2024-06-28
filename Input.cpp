098a75198db14add7a1378232dd7489cbda822d1
n4o8 <146913762+n4o-4@users.noreply.github.com>
2024-06-28 18:19:54 +0900

    02_01_確認課題

---
+++ Input.cpp
@@ -0,0 +1,24 @@
+﻿#include "Input.h"
+
+void Input::Initialize(HINSTANCE hInstance,HWND hwnd)
+{
+	HRESULT result;
+
+	// DirectInputのインスタンス生成
+	Microsoft::WRL::ComPtr<IDirectInput8> directInput = nullptr;
+	result = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
+	assert(SUCCEEDED(result));
+
+	// キーボードデバイス生成
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
+	result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(result));
+
+	// 入力データ形式のセット
+	result = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(result));
+
+	// 排他制御レベルのセット
+	result = keyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(result));
+}
---
+++ Input.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <Windows.h>
+#include <array>
+#include <vector>
+#include <wrl.h>
+
+#include <XInput.h>
+#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
+#include <dinput.h>
+#include<cassert>
+
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+using namespace Microsoft::WRL;
+
+// 入力
+class Input
+{
+public: // メンバ変数
+	// 初期化
+	void Initialize(HINSTANCE hInstance,HWND hwnd);
+
+	// 更新
+	void Update();
+
+private:
+};
+
