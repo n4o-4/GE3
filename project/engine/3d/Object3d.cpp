43383102a60a31340d3b3dc452c72e53f3b89868
n4o8 <146913762+n4o-4@users.noreply.github.com>
2024-07-08 16:49:36 +0900

    07_03_確認課題

---
+++ project/engine/3d/Object3d.cpp
@@ -0,0 +1,56 @@
+#include "Object3d.h"
+#include "MyMath.h"
+#include "TextureManager.h"
+
+void Object3d::Initialize(Object3dCommon* object3dCommon)
+{
+	this->object3dCommon = object3dCommon;
+
+	transformationResource = this->object3dCommon->GetDxCommon()->CreateBufferResource(sizeof(TransformationMatrix));
+
+	// データを書き込むためのアドレスを取得
+	transformationResource.Get()->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData));
+
+	//　単位行列を書き込んでおく
+	transformationMatrixData->WVP = MakeIdentity4x4();
+	transformationMatrixData->World = MakeIdentity4x4();
+
+	directionLightResource = this->object3dCommon->GetDxCommon()->CreateBufferResource(sizeof(DirectionLight));
+	directionLightResource.Get()->Map(0, nullptr, reinterpret_cast<void**>(&directionLightData));
+
+	directionLightData->color = { 1.0f,1.0f,1.0f,1.0f };
+	directionLightData->direction = { 1.0f,0.0f,0.0f };
+	directionLightData->intensity = 1.0f;
+
+	transform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+	cameraTransform = { {1.0f,1.0f,1.0f},{0.3f,0.0f,0.0f},{0.0f,4.0f,-10.0f} };
+}
+
+void Object3d::Update()
+{
+	Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
+	Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(WinApp::kClientWidth) / float(WinApp::kClientHeight), 0.1f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+	transformationMatrixData->WVP = worldViewProjectionMatrix;
+	transformationMatrixData->World = worldMatrix;
+}
+
+void Object3d::Draw()
+{
+	object3dCommon->GetDxCommon()->commandList->SetGraphicsRootConstantBufferView(1, transformationResource.Get()->GetGPUVirtualAddress());
+	
+	object3dCommon->GetDxCommon()->commandList->SetGraphicsRootConstantBufferView(3, directionLightResource.Get()->GetGPUVirtualAddress());
+
+	if (model) {
+		model->Draw();
+	}
+}
+
+void Object3d::SetModel(const std::string& filePath)
+{
+	model = ModelManager::GetInstance()->FindModel(filePath);
+}
+
+
