3d6d70ff2c10bb5d85dee841442b975120db5149
宮本 直弥 <k023g0006@g.neec.ac.jp>
2024-12-03 22:31:18 +0900

    CG3評価課題用

--- project/application/scene/TitleScene.cpp
+++ project/application/scene/TitleScene.cpp
@@ -2,35 +2,11 @@
 
 void TitleScene::Initialize()
 {
+	BaseScene::Initialize();
+
 	TextureManager::GetInstance()->LoadTexture("Resources/monsterBall.png");
 	TextureManager::GetInstance()->LoadTexture("Resources/uvChecker.png");
 	TextureManager::GetInstance()->LoadTexture("Resources/fruit_suika_red.png");
-
-	Audio::GetInstance()->SoundLoadWave("Resources/Alarm01.wav");
-
-	Audio::GetInstance()->SoundPlayWave("Resources/Alarm01.wav");
-
-	sprite = std::make_unique<Sprite>();
-
-	sprite->Initialize(SpriteCommon::GetInstance(), "Resources/monsterBall.png");
-
-	sprite->SetTexSize({ 1200.0f,600.0f });
-
-	sprite->SetAnchorPoint({ 0.5f,0.5f });
-
-	sprite->SetPosition({ 640.0f,360.0f });
-
-	sprite->SetSize({ 1280.0f,720.0f });
-
-	ModelManager::GetInstance()->LoadModel("axis.obj");
-
-	/*object3d = std::make_unique<Object3d>();
-
-	object3d->Initialize(Object3dCommon::GetInstance());
-
-	object3d->SetModel("axis.obj");
-
-	object3d->SetCamera(camera.get());*/
 
 	ParticleManager::GetInstance()->CreateParticleGroup("Particle_1", "Resources/circle.png");
 
@@ -40,42 +16,14 @@ void TitleScene::Initialize()
 	particleEmitter_1->Emit();
 
 	ParticleManager::GetInstance()->SetBlendMode("Add");
-
 }
 
 void TitleScene::Finalize()
 {
-
-	Audio::GetInstance()->SoundStop("Resources/Alarm01.wav");
-
-
 }
 
 void TitleScene::Update()
 {
-	if (Input::GetInstance()->Triggerkey(DIK_RETURN))
-	{
-		SceneManager::GetInstance()->ChangeScene("GAME");
-
-		return;
-	}
-
-	sprite->Update();
-
-	/*Vector3 rotato = object3d->GetRotation();
-
-	rotato.y += 0.02f;
-
-	object3d->SetRotation(rotato);
-
-	Vector3 translate = object3d->GetTranslate();
-
-	translate.z += 0.1f;
-
-	object3d->SetTranslate(translate);
-
-	object3d->Update();*/
-
 	ParticleManager::GetInstance()->Update();
 	particleEmitter_1->Update();
 }
